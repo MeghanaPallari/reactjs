885ee7bba0eea7f62f76ae30abca154b6a7a9b25
MeghanaPallari <meghanapallari@gmail.com>
2025-02-06 11:40:15 +0530

    Add files via upload

---
+++ Inline.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inline = () => {
+  return (
+    <>
+    <h1 style={{backgroundColor : "pink" , textAlign:"center" , textDecoration:"underline" , textDecorationColor:"yellow"}}> Hello Meghana</h1>
+    </>
+  )
+}
+
+export default Inline
