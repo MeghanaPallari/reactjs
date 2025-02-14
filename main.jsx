996d57ee7080f621f8d29fd70958320ffa19b0bd
MeghanaPallari <meghanapallari@gmail.com>
2025-02-14 11:41:44 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,35 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import UseEffect from './useEffect/UseEffect'
-// import CompA from './useContext/CompA'
-// import External from './styles/External'
-// import Ref from './useRef/Ref'
-// import List from './list-keys/List' 
-
-// import './index.css'
-// import State from './state/State.jsx'
-// import Inline from './styles/Inline.jsx'
-//import Cbc from './components/Cbc.jsx'
-//import Demo from './components/Demo.jsx'
-// import Parent from './props/Parent.jsx'
-// import "../src/styles/globally.css"
-// import Comp1 from './styles/Comp1'
-// import Comp2 from './styles/Comp2'
-
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import Master from './component/Master.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/*<Cbc/>}
-    {/* <Demo/> */}
-    {/* <Parent/> */}
-    {/* <Inline/> */}
-    {/* <State/> */}
-    {/* <Comp1/>
-    <Comp2/> */}
-    {/* <External/> */}
-    {/* <Ref /> */}
-    {/* <List /> */}
-    {/* <CompA/> */}
-    <UseEffect/>
+ <BrowserRouter>
+ <Master/>
+ </BrowserRouter>
   </StrictMode>,
 )
